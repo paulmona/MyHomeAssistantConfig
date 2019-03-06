e4e285c1b64971a8ad4cc2eda1202f0a35c51aae
Paul Monaghan <paulm@spider.org>
2019-03-06 17:32:55 -0500

    tested docker run command

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 docker pull homeassistant/home-assistant:latest
 docker stop home-assistant
 docker rm home-assistant
-/usr/bin/docker run -d -t \
+/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker run -d -t \
     -v /share/Public/Containers/HomeAssistant:/config \
     -v /etc/localtime:/etc/localtime:ro \
-    --net=qnet-static-eth0-e5ddec \
+    --net qnet-static-eth0-e5ddec \
     --ip=192.168.0.6 \
-    --hostname=homeassistant
-    --name homeassistant homeassistant/home-assistant:latest python -m homeassistant --config /config --log-rotate-days '3'
+    --hostname=homeassistant \
+    --name homeassistant homeassistant/home-assistant:latest python -m homeassistant --config /config --log-otate-days '3'
