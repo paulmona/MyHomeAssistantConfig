a35d69d24636d5d3087e2722775b00e89ba0598e
Paulmoma <paulm@spider.org>
2019-03-07 05:49:00 -0500

    typo in name

--- ha-dockerrun.sh
+++ ha-dockerrun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker pull homeassistant/home-assistant:latest
-docker stop home-assistant
-docker rm home-assistant
+docker stop homeassistant
+docker rm homeassistant
 /share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker run -d -t \
     -v /share/Public/Containers/HomeAssistant:/config \
     -v /etc/localtime:/etc/localtime:ro \
     --net qnet-static-eth0-e5ddec \
     --ip=192.168.0.6 \
     --hostname=homeassistant \
-    --name homeassistant homeassistant/home-assistant:latest python -m homeassistant --config /config --log-otate-days '3'
+    --name homeassistant homeassistant/home-assistant:latest python -m homeassistant --config /config --log-rotate-days '3'
