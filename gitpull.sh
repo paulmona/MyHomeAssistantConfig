1a861267b69f604209f377dc1076dc017904f940
paulmona <paulm@spider.org>
2019-03-08 11:41:14 -0500

    .

--- gitpull.sh
+++ gitpull.sh
@@ -4,4 +4,4 @@ cd /share/Public/Containers/HomeAssistant
 touch /share/Public/Containers/HomeAssistant/lastpull
 date > /share/Public/Containers/HomeAssistant/lastpull
 /usr/bin/git pull >> /share/Public/Containers/HomeAssistant/lastpull
-/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker restart home-assistant
+/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker restart homeassistant
