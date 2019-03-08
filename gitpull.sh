d6bef4efe2eaa2abde6c1784a66f33cad85705ee
paulmona <paulm@spider.org>
2019-03-08 12:54:37 -0500

    Added new webhook for slack

--- gitpull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /share/Public/Containers/HomeAssistant
-touch /share/Public/Containers/HomeAssistant/lastpull
-date > /share/Public/Containers/HomeAssistant/lastpull
-/usr/bin/git pull >> /share/Public/Containers/HomeAssistant/lastpull
-/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker restart homeassistant
