a59aefc8adcf4c58010b1e05804b3a6b521e1eb3
Paul Monaghan <paulm@spider.org>
2020-02-04 00:04:12 -0500

    Fixed qnet netwok

--- mariadb-dockerrun.sh
+++ mariadb-dockerrun.sh
@@ -5,7 +5,7 @@ docker rm mariadb
 /share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker run -d -t \
     -v /share/Public/Containers/hamariadb/etc:/etc/mysql/conf.d \
     -v /share/Public/Containers/hamariadb:/var/lib/mysql \
-    --net qqnet-static-eth1-08c327 \
+    --net qnet-static-eth1-08c327 \
     --ip=192.168.3.8 \
     --hostname=mariadb \
     --restart=always \
